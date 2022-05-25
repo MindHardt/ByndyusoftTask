d7a2f2afc6faa8ede2940c9232c83ba812214553
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-05-26 03:28:09 +0500

    Добавьте файлы проекта.

---
+++ ByndyusoftTask.Tests/TaskTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using static ByndyusoftTask.Task;
+
+namespace ByndyusoftTask.Tests
+{
+    public class TaskTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 })]
+        [InlineData(new int[] { -5, -4, -3, -2, -1 })]
+        [InlineData(new int[] { 0, 0, 0, 0, 0 })]
+        public void SumOfTwoMinimals_ValuesOK_ReturnOK(int[] value)
+        {
+            int expected = value[0] + value[1];
+            int actual = SumOfTwoMinimals(value);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SumOfTwoMinimals_ValueInvalid_Throw()
+        {
+            Assert.Throws<ArgumentNullException>(() => SumOfTwoMinimals(null!));
+            Assert.Throws<ArgumentException>(() => SumOfTwoMinimals(new int[1]));
+        }
+
+        [Theory]
+        [InlineData(-500, 1000)]
+        [InlineData(int.MinValue, 100_000_000)]
+        public void SumOfTwoMinimals_ValueBig_ReturnOK(int min, int count)
+        {
+            IEnumerable<int> testData = Enumerable.Range(min, count);
+
+            long expected = min + (long)min + 1;
+            long actual = SumOfTwoMinimals(testData);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ ByndyusoftTask/Task.cs
@@ -0,0 +1,28 @@
+﻿namespace ByndyusoftTask
+{
+    public static class Task
+    {
+        //I made it IEnumerable to optimise and allow lazy collections
+        /// <summary>
+        /// Returns a sum of two smallest numbers in <paramref name="collection"/>.
+        /// </summary>
+        /// <param name="collection">A collection of elements</param>
+        /// <returns>The sum of two smallest numbers as <see cref="long"/>.</returns>
+        /// <exception cref="ArgumentNullException">If array is null.</exception>
+        /// <exception cref="ArgumentException">If array has less then 2 elements.</exception>
+        public static long SumOfTwoMinimals(IEnumerable<int> collection)
+        {
+            if (collection is null) throw new ArgumentNullException(nameof(collection));
+            if (collection.Count() < 2) throw new ArgumentException("Array must have at least 2 elements.", nameof(collection));
+
+            int[] minimums = collection
+                .OrderBy(n => n)
+                .Take(2)
+                .ToArray();
+            //I will consider LINQ more optimised than what I could create
+
+            //This is converted to long so that adding two extremely small numbers doesn't lead to overflow
+            return minimums[0] + (long)minimums[1];
+        }
+    }
+}
